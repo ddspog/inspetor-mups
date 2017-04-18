8c4ad290006968069995aa1f2a59041f6336f13d
Dennis Dantas <ddspog@gmail.com>
2017-04-18 20:30:35 -0300

    Add optional way to use GridFS :construction:
    I've added code to collections, to just test how it would be using
    GridFS with Meteor-files.

--- imports/api/images/collection.js
+++ imports/api/images/collection.js
@@ -1,8 +1,102 @@
 import { Meteor } from 'meteor/meteor';
+import { FilesCollection } from 'meteor/ostrio:files';
+import Grid from 'gridfs-stream';
 import { Mongo } from 'meteor/mongo';
 
-export const Images = new Mongo.Collection('images');
+let gfs;
+if (Meteor.isServer) {
+    gfs = Grid(
+        Mongo.defaultRemoteCollectionDriver().mongo.db,
+        Mongo.NpmModule
+    )
+}
+
+export const Images = new FilesCollection({
+    collectionName: 'images',
+    allowClientCode: false,
+    debug: Meteor.isServer && process.env.NODE_ENV === 'development',
+    onBeforeUpload: function (file) {
+        if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+            return true;
+        } else {
+            return 'Please upload image, with size equal or less than 10MB';
+        }
+    },
+    onAfterUpload: function (image) {
+        // Move file to GridFS
+        Object.keys(image.versions)
+              .forEach(versionName => {
+                const metadata = {
+                    versionName,
+                    imageId: image._id,
+                    storedAt: new Date()
+                };
+                const writeStream = gfs.createWriteStream({
+                    filename: image.name,
+                    metadata
+                });
+
+                gfs.createReadStream(image.versions[versionName].path)
+                   .pipe(writeStream);
+
+                writeStream.on('close', Meteor.bindEnvironment(file => {
+                    const property = `versions.${versionName}.meta.gridFsFileId`;
+
+                    // If we store the ObjectID itself, Meteor seems to convert it to a
+                    // LocalCollection.ObjectID, which GFS doesn't undestand.
+                    this.collection.update(
+                        image._id, {
+                            $set: {
+                                [property]: file._id.toString()
+                            }
+                        }
+                    );
+                    this.unlink(
+                        this.collection.findOne(image._id),
+                        versionName
+                    ); // Unlink files from FS
+                }));
+              });
+    },
+    interceptDownload: function (http, image, versionName) {
+        // Serve files from GrifFS
+        const _id = (image.versions[versionName].meta || {}).gridFsFileId;
+        if (_id) {
+            const readStream = gfs.createReadStream({
+                _id
+            });
+            readStream.on('error', err => {
+                throw err;
+            });
+            readStream.pipe(http.response);
+        }
+        return Boolean(_id); // Serve file from GridFS
+    },
+    onAfterRemove: function (images) {
+        // Remove corresponding file from GridFS
+        images.forEach(image => {
+            Object.keys(image.versions)
+                  .forEach(versionName => {
+                      const _id = (image.versions[versionName].meta || {}).gridFsFileId;
+                      if (_id) {
+                          gfs.remove(
+                              { _id },
+                              err => {
+                                  if (err) {
+                                      throw  err;
+                                  }
+                              }
+                          );
+                      }
+                  });
+        });
+    }
+});
 export const Thumbs = new Mongo.Collection('thumbs');
+
+if (Meteor.isServer) {
+    Images.denyClient();
+}
 
 function loggedIn(userId) {
   return !!userId;
