3624862a9894e54f12780a0b0e7ab5687cfb91df
Dennis Dantas <ddspog@gmail.com>
2017-05-22 19:06:40 -0300

    Add geolocation calculation :bulb:
    I've added a working function that calculates geolocation of the
    cellphone executing the app.

--- imports/ui/components/partyAddForm/partyAddForm.js
+++ imports/ui/components/partyAddForm/partyAddForm.js
@@ -4,6 +4,9 @@ import angularMeteor from 'angular-meteor';
 import {
     Meteor
 } from 'meteor/meteor';
+import {
+    Tracker
+} from 'meteor/tracker';
 
 import webTemplate from './web.html';
 import mobileTemplate from './mobile.html';
@@ -31,69 +34,96 @@ class PartyAddForm {
             }
         };
 
-        this.uploadPicture = undefined;
-
         this.areas = GetSanityAreas();
 
         this.helpers({
            subAreaList() {
-               var index_area = this.getReactively('party.type.area');
+               let index_area = this.getReactively('party.type.area');
 
-               if(index_area != null && index_area > -1 && index_area < 5) {
-                   var area = this.areas[index_area];
+               if(index_area !== null && index_area > -1 && index_area < 5) {
+                   let area = this.areas[index_area];
                    return area.subAreas;
                } else {
                    return undefined;
                }
            },
            subAreaDescription() {
-               var index_area = this.getReactively('party.type.area');
-               var index_subArea = this.getReactively('party.type.subArea');
+               let index_area = this.getReactively('party.type.area');
+               let index_subArea = this.getReactively('party.type.subArea');
 
                if(index_subArea){
-                   var area = this.areas[index_area];
-                   var subArea = area.subAreas[index_subArea];
+                   let area = this.areas[index_area];
+                   let subArea = area.subAreas[index_subArea];
 
                    return subArea.description;
                } else {
                    return 'Escolha uma subárea para o problema registrado.';
                }
+           },
+           locationFound() {
+               return !!this.getReactively('party.position');
+           },
+           latitude() {
+               return this.getReactively('party.position.lat');
+           },
+           longitude() {
+                return this.getReactively('party.position.lng');
+           }
+        });
+
+        let localization = new ReactiveVar(null);
+
+        Tracker.autorun((computation) => {
+           localization.set(Geolocation.latLng());
+           if(localization.get()){
+               this.setLocation(localization.get());
+               computation.stop();
            }
         });
     }
 
-    learnUpload(upload) {
-        this.uploadPicture = upload;
-    }
-
     submit() {
         if (!this.uploader) {
-            console.log('Missing uploader on Form.')
+            this.setError("A foto não foi configurada.");
         } else {
-            self = this;
+            if(!this.party.position){
+                this.setError("A posição não foi ajustada.");
+            } else {
+                self = this;
 
-            this.uploader.subscribe("add-party-form", this.$bindToContext((error, fileId) => {
-                if(error) {
+                this.uploader.subscribe("add-party-form", this.$bindToContext((error, fileId) => {
+                    if(error) {
+                        self.setError('Houve um problema durante o upload da foto. Tente de novo.');
+                    } else {
 
-                } else {
-                    console.log('Saving id ' + fileId + ' of image uploaded...');
+                        self.party.image = fileId;
+                        self.party.owner = Meteor.userId();
 
-                    self.party.position = Geolocation.latLng();
-                    self.party.image = fileId;
-                    self.party.owner = Meteor.userId();
+                        Parties.insert(self.party);
 
-                    Parties.insert(self.party);
+                        if (self.done) {
+                            self.done();
+                        }
 
-                    if (self.done) {
-                        self.done();
+                        self.reset();
                     }
+                }));
 
-                    self.reset();
-                }
-            }));
-
-            this.uploader.start();
+                this.uploader.start();
+            }
         }
+    }
+
+    setLocation(position) {
+        this.$bindToContext(() => {
+            this.party.position = position;
+        })();
+    }
+
+    setError(message) {
+        this.$bindToContext(() => {
+            this.error = message;
+        })();
     }
 
     updateSubArea() {
