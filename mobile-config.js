1656278f07320537607474eb4bc3036f3a287c4b
Dennis Dantas <ddspog@gmail.com>
2017-06-11 20:52:12 -0300

    Add icons and title to app :rocket:
    I've put the name of the app, on the app. Also, I've created an icon
    for the app, to identify it better.
    
    Fix #3

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
     name: 'Inspetor Mups',
-    description: 'Tool for recording sanitation problems',
+    description: 'Ferramenta para registrar problemas sanitários.',
     author: 'Dênnis Dantas de Sousa',
     email: 'ddspog@gmail.com'
 });
@@ -11,3 +11,26 @@ App.accessRule('*://ajax.googleapis.com/*');
 App.accessRule('*://fonts.googleapis.com/*');
 
 App.accessRule('data:*', { type: 'navigation' });
+
+App.setPreference('BackgroundColor', '0xffffffff');
+App.setPreference("SplashMaintainAspectRatio", true, "android");
+
+// Set up resources such as icons and launch screens.
+App.icons({
+    'android_mdpi': 'resources/icons/icon-android_mdpi.png',
+    'android_hdpi': 'resources/icons/icon-android_hdpi.png',
+    'android_xhdpi': 'resources/icons/icon-android_xhdpi.png',
+    'android_xxhdpi': 'resources/icons/icon-android_xxhdpi.png',
+    'android_xxxhdpi': 'resources/icons/icon-android_xxxhdpi.png'
+});
+
+App.launchScreens({
+    'android_mdpi_landscape': 'resources/splash/splash-android_mdpi_landscape.png',
+    'android_hdpi_landscape': 'resources/splash/splash-android_hdpi_landscape.png',
+    'android_xhdpi_landscape': 'resources/splash/splash-android_xhdpi_landscape.png',
+    'android_xxhdpi_landscape': 'resources/splash/splash-android_xxhdpi_landscape.png',
+    'android_mdpi_portrait': 'resources/splash/splash-android_mdpi_portrait.png',
+    'android_hdpi_portrait': 'resources/splash/splash-android_hdpi_portrait.png',
+    'android_xhdpi_portrait': 'resources/splash/splash-android_xhdpi_portrait.png',
+    'android_xxhdpi_portrait': 'resources/splash/splash-android_xxhdpi_portrait.png'
+});
